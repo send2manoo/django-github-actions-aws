15e1b437b8e532fb69ef2c0005bc2f5dc4353e42
popmagicnutrifoods <114756156+popmagicnutrifoods@users.noreply.github.com>
2022-10-16 16:37:24 +0530

    Modified views.py to display my name in AWS beanstalk

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>our mind blowing home page</h1>"""
+   text = """<h1>Hi Manohar you done sucessfully deployed AWS beanstalk with S3 bucket using Github Actons!. :)</h1>"""
    return HttpResponse(text)
